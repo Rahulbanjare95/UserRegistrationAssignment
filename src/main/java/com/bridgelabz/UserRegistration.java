2a7a48ae9ec042e3bcda744ef0c376748e34f702
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-20 10:27:18 +0530

    Modified:Managed Indentations

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,25 +5,21 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     boolean match = true;
     public static final String FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+    public static final String LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+    public static final String EMAIL_REGEX_SIMPLEST = "^(.+)@(.+)$";
+    public static final String EMAIL_REGEX_USERNAME = "^[A-Za-z0-9+_.-]+@(.+)$";
+    public static final String EMAIL_REGEX_COMPLETE = "^[A-za-z0-9]+([._+-][0-9a-z]+)@+[A-Za-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)";
+    public static final String PHONE_NUMBER = "^[91]{2}[: :][0-9]{10}$";
+    public static final String PASSWORD = "(?=.*[0-9])(?=.*[A-Z])(?=.*\\d{1})(?=.*[@#$%!]{1}).{8,}";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(FIRST_NAME_REGEX);
         return pattern.matcher(firstName).matches();
-
-
     }
-
-    public static final String LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
-
     public boolean validateLastName(String lastName) {
         Pattern pattern = Pattern.compile(LAST_NAME_REGEX);
         return pattern.matcher(lastName).matches();
     }
-
-    public static final String EMAIL_REGEX_SIMPLEST = "^(.+)@(.+)$";
-    public static final String EMAIL_REGEX_USERNAME = "^[A-Za-z0-9+_.-]+@(.+)$";
-    public static final String EMAIL_REGEX_COMPLETE = "^[A-za-z0-9]+([._+-][0-9a-z]+)@+[A-Za-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)";
-
     public boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_REGEX_SIMPLEST);
         Pattern pattern1 = Pattern.compile(EMAIL_REGEX_USERNAME);
@@ -32,23 +28,13 @@ public class UserRegistration {
             return match;
         } else
             return (!match);
-
-
     }
-
-    public static final String PHONE_NUMBER = "^[91]{2}[: :][0-9]{10}$";
-
     public boolean validatePhoneNumber(String phonenNumber) {
         Pattern pattern = Pattern.compile(PHONE_NUMBER);
         return pattern.matcher(phonenNumber).matches();
-
     }
-
-    public static final String PASSWORD = "(?=.*[0-9])(?=.*[A-Z])(?=.*\\d{1})(?=.*[@#$%!]{1}).{8,}";
-    public boolean validatePassword(String password) {
+     public boolean validatePassword(String password) {
         Pattern pattern=Pattern.compile(PASSWORD);
         return pattern.matcher(password).matches();
-
     }
-
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,22 +1,20 @@
 package com.bridgelabz;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
     @Test
     public void givenFirstName_WhenCorrect_ShouldReturnTrue() {
 
-       UserRegistration userRegistration =new UserRegistration();
-       boolean isValid = userRegistration.validateFirstName("Rahul");
+        UserRegistration userRegistration = new UserRegistration();
+        boolean isValid = userRegistration.validateFirstName("Rahul");
         Assert.assertTrue(isValid);
     }
     @Test
     public void givenFirstName_WhenInCorrectlength_ShouldReturnFalse() {
 
-        UserRegistration userRegistration =new UserRegistration();
+        UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validateFirstName("Ra");
         Assert.assertFalse(isValid);
     }
@@ -50,63 +48,54 @@ public class UserRegistrationTest {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validateEmail("abc@yahoo.com");
         Assert.assertTrue(isValid);
-
     }
     @Test
     public void givenEmail_WithoutAtSymbol_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validateEmail("@yahoo.com");
         Assert.assertFalse(isValid);
-
     }
     @Test
     public void givenEmail_WithProperNames_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validateEmail("abc.qax@yahoo.co.in");
         Assert.assertTrue(isValid);
-
     }
     @Test
     public void givenMobile_WithProperFormat_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validatePhoneNumber("91 8982423482");
         Assert.assertTrue(isValid);
-
     }
     @Test
     public void givenMobile_WithoutSpace_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validatePhoneNumber("91982423482");
         Assert.assertFalse(isValid);
-
     }
     @Test
     public void givenMobile_WithlessLength_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validatePhoneNumber("91982");
         Assert.assertFalse(isValid);
-
     }
     @Test
     public void givenPassword_WithMin8charchter_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validatePassword("122abcde#fghAAS");
         Assert.assertTrue(isValid);
-
     }
     @Test
     public void givenPassword_WithLessCharacters_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validatePassword("zx2A");
         Assert.assertFalse(isValid);
-
     }
     @Test
     public void givenPassword_WithNoUpperCase_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validatePassword("zx2");
         Assert.assertFalse(isValid);
-
     }
     @Test
     public void givenPassword_WithNoUpperCaseButProper_ShouldReturnFalse() {
@@ -126,12 +115,4 @@ public class UserRegistrationTest {
         boolean isValid = userRegistration.validatePassword("Aa12");
         Assert.assertFalse(isValid);
     }
-
-
-
-
-
 }
-
-
-
